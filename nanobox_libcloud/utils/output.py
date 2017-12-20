01dd64b8531338131031bc036829b969ee618f45
Daniel Hunsaker <danhunsaker@gmail.com>
2017-12-20 13:21:50 -0600

    [tests] Test Utilities
    Completed:
    - `meta` controller (integration tests)
    - `keys` controller (integration tests)
    - `servers` controller (integration tests)
    - `models` utility (unit tests)
    - `output` utility (unit tests)
    
    TODO:
    - all adapters (unit tests)
    - all tasks (unit tests)

--- nanobox_libcloud/utils/output.py
+++ nanobox_libcloud/utils/output.py
@@ -11,8 +11,14 @@ def success(data, status=200) -> FlaskResponse:
 
 
 def failure(message, status=400) -> FlaskResponse:
-    return json.dumps({"errors": ([message]\
-           if hasattr(message, 'strip') else message if hasattr(message, 'split')\
-           else repr(message))}, indent=2),\
+    return json.dumps({"errors": (
+                       [message] if hasattr(message, 'strip')\
+                                 else message if message is None\
+                                              or (isinstance(message, dict)\
+                                                  and not hasattr(message, 'errors'))\
+                                              else message['errors']\
+                                                  if isinstance(message, dict)\
+                                                  else repr(message)
+                      )}, indent=2),\
            status,\
            [("Content-Type", "application/json")]
