831abc092521762ce8ebaa5ddd5a4570a19b7972
Daniel Hunsaker <danhunsaker@gmail.com>
2017-12-25 23:47:14 -0700

    [tests] Base Adapter
    Wanted to test even the base adapter, mostly to cut down on the sheer amount of stuff being tested in each adapter's own implementation. If it works in the base, and we don't override it in the provider-specific, then we shouldn't have to test it again there.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+import libcloud
 import nanobox_libcloud
 import os
 import unittest
@@ -7,42 +8,59 @@ from flask import json
 
 
 class NanoboxLibcloudTestCase(unittest.TestCase):
+    adapters = None
+    creds = None
+    patches = []
     skip_adapters = ['azure']
+    test_key = None
 
     def setUp(self):
         nanobox_libcloud.app.testing = True
         self.app = nanobox_libcloud.app.test_client()
 
-        with open(os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            'cred_evars.json'
-        )) as fp:
-            content = fp.read()
-        creds = json.loads(content)
-        self.adapters = sorted(nanobox_libcloud.adapters.base.AdapterBase.registry.keys())
-        for a in self.skip_adapters:
-            self.adapters.remove(a)
-        self.creds = {adapter: {
-            header: os.getenv(evar, '').replace('\n', '\\n')
-            for header, evar in creds.get(adapter, {}).items()
-        } for adapter in self.adapters}
+        if not self.adapters or not self.creds:
+            with open(os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                'cred_evars.json'
+            )) as fp:
+                creds = json.loads(fp.read())
 
-        with open(os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            'id_rsa.pub'
-        )) as fp:
-            self.test_key = fp.read()
+            self.adapters = sorted(nanobox_libcloud.adapters.base.AdapterBase.registry.keys())
+            for a in self.skip_adapters:
+                self.adapters.remove(a)
+
+            self.creds = {adapter: {
+                header: os.getenv(evar, '').replace('\n', '\\n')
+                for header, evar in creds.get(adapter, {}).items()
+            } for adapter in self.adapters}
+
+        if not self.test_key:
+            with open(os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                'id_rsa.pub'
+            )) as fp:
+                self.test_key = fp.read()
 
         with nanobox_libcloud.app.app_context():
             warnings.simplefilter("ignore", ResourceWarning)
             pass
 
     def tearDown(self):
-        pass
+        for path, original in self.patches:
+            exec('%s = original' % path)
+
+        self.patches = []
+
+    def patch(self, original, mock):
+        path = '%s.%s' % (original.__module__, original.__qualname__)
+        self.patches.append((path, original))
+        exec('%s = mock' % path)
+        # original = mock
 
 
 if __name__ == '__main__':
-    unittest.defaultTestLoader.loadTestsFromModule('tests.controllers')
+    unittest.defaultTestLoader.loadTestsFromModule('tests.utils')
     unittest.defaultTestLoader.loadTestsFromModule('tests.adapters')
     unittest.defaultTestLoader.loadTestsFromModule('tests.tasks')
+    unittest.defaultTestLoader.loadTestsFromModule('tests.controllers')
     unittest.main()
